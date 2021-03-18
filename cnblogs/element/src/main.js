44e638735185e5eee0b771c8b1137a2d02b9ebc4
Your Name <you@example.com>
2021-03-18 16:10:08 +0800

    自定义指令

--- cnblogs/element/src/main.js
+++ cnblogs/element/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import '@/utils/directive.js' // 自定义指令
+import '@/utils/filters.js'   //  过滤器
 
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
   render: h => h(App),
